32d497d00e6270dc9d4dea851fc47347fc53cc47
thomsonvrinfo-commits <thomsonvr.info@gmail.com>
2026-09-10 02:27:22 -0600

    Refactor authentication check logic in AuthContext

--- src/lib/AuthContext.jsx
+++ src/lib/AuthContext.jsx
@@ -27,9 +27,9 @@ export const AuthProvider = ({ children }) => {
         setAppPublicSettings(publicSettings);
         
         // If we got the app public settings successfully, check if user is authenticated
-        if (appParams.token) {
-          await checkUserAuth();
-        } else {
+        if (zimrent.auth.isAuthenticated()) {
+        await checkUserAuth();
+      } else {
           setIsLoadingAuth(false);
           setIsAuthenticated(false);
           setAuthChecked(true);
